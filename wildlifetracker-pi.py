f94b29db4b728c0666561b3ff179d8adb314628a
Saad-Imtiaz <saadimtiazsial@gmail.com>
2023-12-05 14:01:01 +0500

    V4.1
    #Updated V4+1
    # This one is better than the V3 (last commit) as this one compares the lastest image and the last image taken by the raspberry pi -
    # However if the same person moves in the frame he is gets detected multiple times as well
    # At the end of the code we get a print out of all the sum of objects collected per category.

--- wildlifetracker-pi.py
+++ wildlifetracker-pi.py
@@ -1,5 +1,7 @@
-# This code is working but one object is getting detected multiple times in one second. 
-
+#Updated V4+1
+# This one is better than the  V3 as this one compares the lastest image and the last image taken by the raspberry pi - 
+# However if the same person moves in the frame he is gets detected multiple times as well
+# At the end of the code we get a print out of all the sum of objects collected per category.
 import cv2
 import pandas as pd
 import numpy as np
@@ -29,29 +31,26 @@ def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
     cv2.putText(img, label, (x-10, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
+# Function to calculate centroid of a bounding box
+def calculate_centroid(x, y, w, h):
+    return (int(x + w/2), int(y + h/2))
+
 # Function to capture image using libcamera-still
 def capture_image(image_path):
-    subprocess.run(["libcamera-still", "-o", image_path, "--width", "1280", "--height", "720", "-n", "-t", "1"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    subprocess.run(["libcamera-still", "-o", image_path, "--width", "1920", "--height", "1080", "-n", "-t", "1"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 # Initialize a DataFrame to store counts
 data_frame = pd.DataFrame(columns=['Timestamp', 'Type', 'Count'])
+object_counts = {cls: 0 for cls in classes}  # Object count per category
+centroid_tracking = {}  # Tracks centroids of detected objects
 
 # Main loop
 image_path = "temp.jpg"
-csv_file_path = "object_counts.csv"  # Path to save CSV file
-
 try:
     while True:
         capture_image(image_path)
-
-        # Check if image is captured correctly
-        if not os.path.exists(image_path):
-            print("Image capture failed, image file not found.")
-            continue
-
         frame = cv2.imread(image_path)
         if frame is None:
-            print("Failed to read the image file.")
             continue
 
         Width = frame.shape[1]
@@ -67,6 +66,7 @@ try:
         class_ids = []
         confidences = []
         boxes = []
+        centroids = []
         conf_threshold = 0.5
         nms_threshold = 0.4
 
@@ -85,17 +85,23 @@ try:
                     class_ids.append(class_id)
                     confidences.append(float(confidence))
                     boxes.append([x, y, w, h])
+                    centroids.append(calculate_centroid(x, y, w, h))
 
         indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
 
         for i in indices:
             box = boxes[i]
-            x = box[0]
-            y = box[1]
-            w = box[2]
-            h = box[3]
-            draw_bounding_box(frame, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
-            data_frame = pd.concat([data_frame, pd.DataFrame([{'Timestamp': datetime.now(), 'Type': classes[class_ids[i]], 'Count': 1}])], ignore_index=True)
+            x, y, w, h = box
+            centroid = centroids[i]
+            class_id = class_ids[i]
+
+            # Check if this object was already detected
+            if class_id not in centroid_tracking or not any(np.linalg.norm(np.array(centroid) - np.array(old_centroid)) < 50 for old_centroid in centroid_tracking[class_id]):
+                object_counts[classes[class_id]] += 1
+                centroid_tracking.setdefault(class_id, []).append(centroid)
+                data_frame = data_frame.append({'Timestamp': datetime.now(), 'Type': classes[class_id], 'Count': 1}, ignore_index=True)
+
+            draw_bounding_box(frame, class_id, confidences[i], round(x), round(y), round(x+w), round(y+h))
 
         # Display the frame
         cv2.imshow("Object Detection", frame)
@@ -108,5 +114,14 @@ finally:
     if os.path.exists(image_path):
         os.remove(image_path)
 
-    # Save counts to CSV
-    data_frame.to_csv(csv_file_path, index=False)
+# Print and save the total count of objects detected per category
+total_counts = pd.DataFrame(object_counts.items(), columns=['Type', 'Total Count'])
+print(total_counts)
+total_counts.to_csv("total_object_counts.csv", index=False)
+
+# Save detailed counts to CSV
+data_frame.to_csv("object_counts.csv", index=False)
+
+# Write the total count to a text log
+with open("total_counts_log.txt", "w") as log_file:
+    log_file.write(str(total_counts))
